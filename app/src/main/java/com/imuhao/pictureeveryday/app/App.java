72a9147c00480af543bae803bce9dbfe638aab85
smile <imuhao@qq.com>
2017-04-26 17:19:46 +0800

    code optimize

--- app/src/main/java/com/imuhao/pictureeveryday/app/App.java
+++ app/src/main/java/com/imuhao/pictureeveryday/app/App.java
@@ -1,20 +1,15 @@
 package com.imuhao.pictureeveryday.app;
 
 import android.app.Application;
-import com.lzy.okgo.OkGo;
 
 /**
  * @author Smile
  * @time 2016/6/22  11:24
  * @desc ${TODD}
  */
-public class MyApplication extends Application {
+public class App extends Application {
 
   public void onCreate() {
     super.onCreate();
-    OkGo.init(this);
-
-    //OkGo.getInstance();
-
   }
 }
