0e11e38a36939c31402a47e23c21eddda29cde6c
smile <imuhao@qq.com>
2017-05-04 14:19:54 +0800

    add item animation

--- app/src/main/java/com/imuhao/pictureeveryday/ui/adapter/TodayAdapter.java
+++ app/src/main/java/com/imuhao/pictureeveryday/ui/adapter/TodayAdapter.java
@@ -1,9 +1,12 @@
 package com.imuhao.pictureeveryday.ui.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.imuhao.pictureeveryday.R;
@@ -80,6 +83,8 @@ public class TodayAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
 
   class TitleHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     TextView title;
+    int mLastPosition;
+    private static final int DELAY = 128;
 
     public TitleHolder(View itemView) {
       super(itemView);
@@ -89,11 +94,37 @@ public class TodayAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
 
     public void bind(ItemViewBean itemViewBean) {
       title.setText(itemViewBean.title);
+      showItemAnim(itemView, getAdapterPosition());
     }
 
     @Override public void onClick(View v) {
       ItemViewBean itemViewBean = mData.get(getAdapterPosition());
       WebActivity.start(itemView.getContext(), itemViewBean.title, itemViewBean.url);
+    }
+
+    public void showItemAnim(final View view, final int position) {
+      final Context context = view.getContext();
+      if (position > mLastPosition) {
+        view.setAlpha(0);
+        view.postDelayed(new Runnable() {
+          @Override public void run() {
+            Animation animation = AnimationUtils.loadAnimation(context, R.anim.slide_in_right);
+            animation.setAnimationListener(new Animation.AnimationListener() {
+              @Override public void onAnimationStart(Animation animation) {
+                view.setAlpha(1);
+              }
+
+              @Override public void onAnimationEnd(Animation animation) {
+              }
+
+              @Override public void onAnimationRepeat(Animation animation) {
+              }
+            });
+            view.startAnimation(animation);
+          }
+        }, DELAY * position);
+        mLastPosition = position;
+      }
     }
   }
 
