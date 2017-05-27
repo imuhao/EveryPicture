7d4d1c2a4f07bc46e12fbcdd87749cf522d1ea96
smile <imuhao@qq.com>
2017-05-27 15:13:00 +0800

    修改关于页面颜色

--- app/src/main/java/com/imuhao/pictureeveryday/ui/fragment/AboutFragment.java
+++ app/src/main/java/com/imuhao/pictureeveryday/ui/fragment/AboutFragment.java
@@ -1,25 +1,41 @@
 package com.imuhao.pictureeveryday.ui.fragment;
 
 import android.view.View;
+
 import com.imuhao.pictureeveryday.R;
 import com.imuhao.pictureeveryday.ui.base.BaseFragment;
+import com.imuhao.pictureeveryday.utils.ThemeUtils;
 
 /**
  * @author Smile
  * @time 16/05/2017  16:42
  * @desc ${TODD}
  */
-public class AboutFragment extends BaseFragment{
+public class AboutFragment extends BaseFragment {
 
-  public static AboutFragment newInstance(){
-    return new AboutFragment();
-  }
+    private View bgView;
 
-  @Override protected int getLayoutId() {
-    return R.layout.fragment_about;
-  }
+    public static AboutFragment newInstance() {
+        return new AboutFragment();
+    }
 
-  @Override protected void initView(View view) {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_about;
+    }
 
-  }
+    @Override
+    protected void initView(View view) {
+        bgView = view.findViewById(R.id.bg_view);
+    }
+
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (!hidden) {
+            int themeColor = ThemeUtils.getThemeColor();
+            bgView.setBackgroundColor(themeColor);
+        }
+    }
 }
